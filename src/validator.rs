b3794f02e2a61236dcdd521489c7fe7bce3a34af
nolash <dev@holbrook.no>
2021-12-29 12:50:33 +0000

    Remove unneeded sha2 dev dep

--- src/validator.rs
+++ src/validator.rs
@@ -19,23 +19,23 @@ impl ValidatorCollection {
 
 #[cfg(test)]
 mod tests {
-    use fadafada::validator::{
-        Sha256ImmutableValidator,
-        Validator,
-    };
-    use sha2::{Sha256, Digest};
+    use fadafada::validator::Validator;
+    use fadafada::web2::Sha256ImmutableValidator;
+    use hex;
+
     use env_logger;
+
+    const HASH_OF_FOO: &str = "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae";
 
     #[test]
     fn test_validator_sha256_as_default() {
         env_logger::init();
 
-        let mut h = Sha256::new();
         let content = vec![0x66, 0x6f, 0x6f];
-        h.update(&content);
-        let content_digest: Vec<u8> = h.finalize().to_vec();
+        let content_digest = hex::decode(HASH_OF_FOO).unwrap();
        
         let sha256_validator = Sha256ImmutableValidator{};
         let r = sha256_validator.verify(&content_digest, Some(&content), None);
+        assert!(r);
     }
 }
